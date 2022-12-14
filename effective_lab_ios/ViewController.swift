1f8cf476276828bc1db28cf12db6f721cc19c909
Flobsterable <loki55rus@gmail.com>
2022-12-14 15:34:44 -0800

    upload project

---
+++ effective_lab_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  effective_lab_ios
+//
+//  Created by BANSHEE on 14.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
